dfb6f6bea47b4a6a03cd19b509fce482c39cc0b5
Edward Kim <edwardkim.ek@gmail.com>
2015-10-20 21:43:48 -0400

    Added a basic java template used for competitions

---
+++ template/Main.java
@@ -0,0 +1,11 @@
+// Java template
+import java.util.Scanner;
+public class Main{   
+	public static void main (String[] args){
+        	int i;      
+		Scanner scan = new Scanner (System.in);
+        	while (scan.hasNext()){
+	            i = scan.nextInt();
+	        }
+	}
+}
